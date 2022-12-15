ea33e237c32f66b8e9bb0c726f511158b61e0577
jussaw <jtsaw36@gmail.com>
2022-12-15 00:47:23 -0700

    Update borer and spacing of list items

--- lib/home_page/dog_park_entry.dart
+++ lib/home_page/dog_park_entry.dart
@@ -15,27 +15,44 @@ class DogParkEntry extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      width: double.infinity,
-      child: Row(
-        children: [
-          Column(
-            children: const [
-              Icon(Icons.arrow_upward, color: Colors.black, size: 12),
-              Icon(Icons.arrow_downward, color: Colors.black, size: 12)
-            ],
-          ),
-          Column(
-            children: [
-              Text((likeCount - dislikeCount).toString()),
-            ],
-          ),
-          Column(
-            children: [
-              Text(name),
-            ],
-          ),
-        ],
+    const double sideMargin = 4.0;
+    const double topBottomMargin = 2.0;
+
+    return Container(
+      margin: const EdgeInsets.only(
+        left: sideMargin,
+        right: sideMargin,
+        top: topBottomMargin,
+        bottom: topBottomMargin,
+      ),
+      decoration: BoxDecoration(
+        border: Border.all(color: Colors.black45),
+        borderRadius: BorderRadius.circular(8.0),
+        color: Colors.black12,
+      ),
+      child: SizedBox(
+        width: double.infinity,
+        height: 32.0,
+        child: Row(
+          children: [
+            Column(
+              children: const [
+                Icon(Icons.arrow_upward, color: Colors.black, size: 12),
+                Icon(Icons.arrow_downward, color: Colors.black, size: 12)
+              ],
+            ),
+            Column(
+              children: [
+                Text((likeCount - dislikeCount).toString()),
+              ],
+            ),
+            Column(
+              children: [
+                Text(name),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
