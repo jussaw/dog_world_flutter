93476bc3ffdc39cc174d94bb44ad9c654b40e0a9
jussaw <jtsaw36@gmail.com>
2022-12-15 22:06:43 -0700

    Add dog park details model

---
+++ lib/models/dog_park_details_model.dart
@@ -0,0 +1,13 @@
+class DogParkDetailsModel {
+  DogParkDetailsModel({
+    required this.name,
+    required this.description,
+    required this.likeCount,
+    required this.dislikeCount,
+  });
+
+  final String name;
+  final String description;
+  final int likeCount;
+  final int dislikeCount;
+}
