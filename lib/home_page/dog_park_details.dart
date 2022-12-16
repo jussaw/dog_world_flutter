4895094fd8dbcf991778fc8bf2de0c7564699c5a
jussaw <jtsaw36@gmail.com>
2022-12-15 22:35:31 -0700

    Add comments to details page

--- lib/home_page/dog_park_details.dart
+++ lib/home_page/dog_park_details.dart
@@ -11,12 +11,52 @@ class DogParkDetails extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    const double margin = 10.0;
+    const double padding = 5.0;
+    const double borderRadius = 8.0;
+
     return Scaffold(
       appBar: AppBar(
         leading: ElevatedButton(
           child: const Icon(Icons.arrow_back),
           onPressed: () => {Navigator.of(context).pop()},
         ),
+      ),
+      body: Column(
+        children: [
+          SizedBox(
+            width: double.infinity,
+            child: Container(
+              alignment: Alignment.center,
+              margin: const EdgeInsets.all(margin),
+              padding: const EdgeInsets.all(padding),
+              decoration: BoxDecoration(
+                border: Border.all(color: Colors.black),
+                borderRadius: BorderRadius.circular(borderRadius),
+              ),
+              child: Text(dogParkDetailsData.description),
+            ),
+          ),
+          Text("--------COMMENTS--------"),
+          ...dogParkDetailsData.comments.map((comment) {
+            return SizedBox(
+              width: double.infinity,
+              child: Container(
+                alignment: Alignment.center,
+                margin: const EdgeInsets.all(margin),
+                padding: const EdgeInsets.all(padding),
+                decoration: BoxDecoration(
+                  border: Border.all(color: Colors.black),
+                  borderRadius: BorderRadius.circular(borderRadius),
+                ),
+                child: Text(
+                  style: const TextStyle(fontSize: 6),
+                  comment,
+                ),
+              ),
+            );
+          }),
+        ],
       ),
     );
   }
