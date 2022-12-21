2cdf5f55040d8fb2476d7d432000e55b022fbeb9
jussaw <jtsaw36@gmail.com>
2022-12-21 16:13:17 -0700

    Center error message

--- lib/dog_park_info/providers/dog_park_info.dart
+++ lib/dog_park_info/providers/dog_park_info.dart
@@ -69,18 +69,22 @@ class _DogParkInfoState extends State<DogParkInfo> {
 
             // TODO: Update error message
           } else if (snapshot.hasError) {
-            child = Column(
-              children: [
-                const Icon(
-                  Icons.error_outline,
-                  color: Colors.red,
-                  size: 60,
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(top: 16),
-                  child: Text('Error: ${snapshot.error}'),
-                ),
-              ],
+            child = Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  const Icon(
+                    Icons.error_outline,
+                    color: Colors.red,
+                    size: 60,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 16),
+                    child: Text('Error: ${snapshot.error}'),
+                  ),
+                ],
+              ),
             );
             // TODO: Update progress message
           } else {
